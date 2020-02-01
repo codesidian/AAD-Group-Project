f62e95e8936f415922b53cd0243cda6b53f66c47
William Coates <me@willtc.uk>
2020-02-01 19:54:53 +0000

    Implemented Cart

---
+++ CustomerSite/src/data/Cart.ts
@@ -0,0 +1,7 @@
+import * as Types from "./Types";
+
+export default interface Cart {
+    addItem(item: Types.Item): void;
+    getItems(): ArrayLike<Types.SaleItem>;
+    setItemQuantity(code: string, quantity: number): void;
+}
---
+++ CustomerSite/src/data/MemoryCart.ts
@@ -0,0 +1,31 @@
+import Cart from "./Cart";
+import { Item, SaleItem } from "./Types";
+
+export default class MemoryCart implements Cart {
+    items = new Array<SaleItem>();
+
+    addItem(item: Item): void {
+        let saleItem = this.items.find(x => x.item.code === item.code);
+        if (saleItem === undefined) {
+            saleItem = {item: item, quantity: 0};
+            this.items.push(saleItem);
+        }
+        saleItem.quantity++;
+    }
+
+    getItems(): ArrayLike<SaleItem> {
+        return this.items;
+    }
+
+    setItemQuantity(code: string, quantity: number): void {
+        let saleItem = this.items.find(x => x.item.code === code);
+        if (saleItem === undefined) {
+            throw new Error("Sale item not found");
+        }
+        if (quantity <= 0) {
+            this.items.splice(this.items.indexOf(saleItem), 1);
+        } else {
+            saleItem.quantity = quantity;
+        }
+    }
+}
