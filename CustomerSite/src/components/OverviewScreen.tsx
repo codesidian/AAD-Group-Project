ca2d6c649e46a9ae94e9be7c08dfa0d993d35b0d
William Coates <me@willtc.uk>
2020-02-02 17:27:35 +0000

    Added total to overview screen

--- CustomerSite/src/components/OverviewScreen.tsx
+++ CustomerSite/src/components/OverviewScreen.tsx
@@ -16,9 +16,13 @@ type OverviewScreenProps = {
 
 export default class OverviewScreen extends React.Component<OverviewScreenProps> {
     render() {
+        let totalPrice = 0;
+        Array.from(this.props.cart.getItems()).forEach(x => totalPrice += x.quantity * x.item.price);
+
         return <div>
             <h1>Overview</h1>
             <ProductTable cart={this.props.cart} editable={false} />
+            <div>Total price: £{(totalPrice / 100).toFixed(2)}</div>
             <div className="btn-group" role="group" style={{width: "100%"}}>
                 <button className="btn btn-secondary" onClick={this.props.onBack}>Back</button>
                 <button className="btn btn-primary" onClick={this.props.onNext}>Next</button>
